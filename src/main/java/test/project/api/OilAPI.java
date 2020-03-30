f86ace4a9d503091ab0ba360cc5855c770903362
kateryna-maksymenko <mkaterina3@gmail.com>
2020-03-30 10:40:19 +0200

    change OilAPI. try default method in interface.

--- src/main/java/test/project/api/OilAPI.java
+++ src/main/java/test/project/api/OilAPI.java
@@ -2,16 +2,22 @@ package test.project.api;
 
 import java.util.List;
 
+import com.google.common.cache.RemovalListener;
 import test.project.model.OilType;
 import test.project.model.TransactionRowInfo;
 import test.project.model.TransactionType;
 
 public interface OilAPI {
-	
-	TransactionRowInfo getTransactionInfoBase(double price, OilType oilType, TransactionType transactionType);
-	
-	double getInventoryIndex(List<TransactionRowInfo> transactions);
-	
-	double getVolumeWeightValue(List<TransactionRowInfo> transactions);
-	
+
+    TransactionRowInfo getTransactionInfoBase(double price, OilType oilType, TransactionType transactionType);
+
+    double getInventoryIndex(List<TransactionRowInfo> transactions);
+
+    double getVolumeWeightValue(List<TransactionRowInfo> transactions);
+
+    default void helloWorld() {
+        System.out.println("hello world");
+
+    }
+
 }
